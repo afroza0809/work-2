2e19b0907d88260caeee89fc5c6927cbf2f767ad
afroza0809 <aafroza665@gmail.com>
2022-05-25 15:05:33 +0600

    uploding

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// min to hours
+
+let min = parseInt( prompt('Enter min'));
+
+let hours = min/60;
+
+console.log(hours +"h");
+
+// inechs to miter
+// 1in = 0.0254m
+
+let inches = prompt('input here inches');
+
+let miters = inches * 0.0254;
+
+console.log(miters + "miters");
+
+
+
+
+// BMI
+
+let weight = 46;
+let height = 1.57; 
+// height will be miters
+
+
+let bmi = weight/(height*height);
+
+console.log( "BMI is "  + bmi);
+
+
+
